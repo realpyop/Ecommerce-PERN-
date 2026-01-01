a350cee6df6a1824ecc8e210b75ddc4e8d7a2a02
An Van <anvan.official@gmail.com>
2026-01-01 16:57:13 -0500

    Finishing up backend with CORS, FRONTEND_URL, and Clerk

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,14 @@
 import express from "express"
+import cors from "cors"; 
 import { ENV } from "./config/env"
 import { clerkMiddleware } from "@clerk/express"
 
 const app = express()
 
-app.use(clerkMiddleware());
+app.use(cors({ origin: ENV.FRONTEND_URL })); //Bypass API blocking API call from localhost
+app.use(clerkMiddleware()); // Auth obj will be attached to the req
+app.use(express.json()); // Parses JSON request bodies
+app.use(express.urlencoded({ extended: true })); // Parses form data (like HTML forms)
 
 app.get("/", (req, res) => {
     res.json({
